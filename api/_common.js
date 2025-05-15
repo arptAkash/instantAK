9cfa6881e6a58619f27c006ed6d124308669e391
Akash yadav <125228685+Sayarkhwab@users.noreply.github.com>
2025-05-15 16:25:23 +0530

    Update _common.js

--- api/_common.js
+++ api/_common.js
@@ -3,7 +3,7 @@ export const READABILITY_API_URL = getApiUrlFromEnv();
 export const IV_RHASH = process.env.IV_RHASH; // Track rules.iv in Telegram at first
 export const BOT_TOKEN = process.env.BOT_TOKEN;
 
-export const DEFAULT_USER_AGENT_SUFFIX = "readability-bot/0.0";
+export const DEFAULT_USER_AGENT_SUFFIX = "userInstantBot/0.0";
 export const FALLBACK_USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3 Safari/605.1.15 " + DEFAULT_USER_AGENT_SUFFIX;
 
 function getApiUrlFromEnv() {
@@ -12,7 +12,7 @@ function getApiUrlFromEnv() {
   } else if (process.env.VERCEL_URL) {
     return "https://" + process.env.VERCEL_URL + "/api/readability";
   } else {
-    return "https://readability-bot.vercel.app/api/readability";
+    return "https://arpt.vercel.app/api/readability";
   }
 }
 
